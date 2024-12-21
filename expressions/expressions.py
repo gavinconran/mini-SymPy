4466191523e219a2ebc1932e7a7222a8189ee6cc
gavinconran <gtconran@gmail.com>
2024-12-21 16:49:27 +0000

    import test passed plus flake8 issues

--- expressions/expressions.py
+++ expressions/expressions.py
@@ -1,7 +1,7 @@
 """Contains the Class Expression and associated sub-classes."""
 
-from numbers import Number
-import numpy as np
+import numbers
+
 
 class Expression:
     """Expression class represengts an algebriac expression."""
@@ -12,39 +12,39 @@ class Expression:
 
     def __add__(self, other):
         """Add Expression self to Expression other or number other."""
-        raise NotImplemented
+        raise NotImplementedErro
 
     def __radd__(self, other):
         """Add Expression other to Number self."""
-        raise NotImplemented
+        raise NotImplementedError
 
     def __sub__(self, other):
         """Subtract Expression or Number other from Expression self."""
-        raise NotImplemented
+        raise NotImplementedError
 
     def __rsub__(self, other):
         """Subtract Expression other from Number self."""
-        raise NotImplemented
+        raise NotImplementedError
 
     def __mul__(self, other):
         """Multiply Expression self from Expression other or Number other."""
-        raise NotImplemented
+        raise NotImplementedError
 
     def __rmul__(self, other):
         """Multiply Number self with Expression other."""
-        raise NotImplemented
+        raise NotImplementedError
 
     def __div__(self, other):
         """Divide Expression self by Expression other or Number other."""
-        raise NotImplemented
+        raise NotImplementedError
 
     def __rtruediv__(self, other):
         """Divide Number self with Expression other."""
-        raise NotImplemented
+        raise NotImplementedError
 
     def __pow__(self, other):
         """Raise Expression self to the power Expression or Number other."""
-        raise NotImplemented
+        raise NotImplementedError
 
 
 class Operator(Expression):
@@ -56,46 +56,46 @@ class Operator(Expression):
 
     def __str__(self):
         """Return a string representation of the Operator."""
-        raise NotImplemented
+        raise NotImplementedError
 
 
 class Add(Expression):
-    """Add class"""
+    """Add class."""
 
     precedence = 2
     symbol = '+'
 
 
 class Sub(Expression):
-    """Subtract class"""
+    """Subtract class."""
 
     precedence = 2
-    symbol = '-'    
+    symbol = '-'
 
 
 class Mul(Expression):
-    """Multiply class"""
+    """Multiply class."""
 
     precedence = 1
     symbol = '*'
 
 
 class Div(Expression):
-    """Divide class"""
+    """Divide class."""
 
     precedence = 1
     symbol = '/'
 
 
 class Pow(Expression):
-    """Power class"""
+    """Power class."""
 
     precedence = 3
     symbol = '+'
 
 
 class Terminal(Expression):
-    """Terminal Class"""
+    """Terminal Class."""
 
     def __init__(self, operands):
         """Operator class constructor method."""
@@ -111,26 +111,20 @@ class Terminal(Expression):
 
 
 class Number(Expression):
-    """Number class"""
+    """Number class."""
 
     def __init__(self, value):
-        """Number class constructor method."""
-        if isinstance(value, Number):
+        """Class (Number) constructor method."""
+        if isinstance(value, numbers.Number):
             self.value = value
         return super().__init__(())
 
 
 class Symbol(Expression):
-    """Symbol class"""
+    """Symbol class."""
 
     def __init__(self, value):
         """Symbol class constructor method."""
         if isinstance(value, str):
             self.value = value
         return super().__init__(())
-
-
-
-
-
-
