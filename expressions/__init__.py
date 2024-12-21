9b44ac9a36345925667b4b88f247f953050217a4
gavinconran <gtconran@gmail.com>
2024-12-21 17:00:16 +0000

    import test passed plus flake8 issues

--- expressions/__init__.py
+++ expressions/__init__.py
@@ -1,3 +1,3 @@
 """Contains the expressions __init__ file for Chp 9 of OOP4Maths."""
 from .expressions import Number, Symbol, \
-    Add, Sub, Mul, Div, Pow
+    Add, Sub, Mul, Div, Pow    # noqa F401
