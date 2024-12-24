12b422e4bc664d96cf51d0e5b4261fb5c2e2a045
gavinconran <gtconran@gmail.com>
2024-12-24 17:06:49 +0000

    basic Operator tests passing with flake8 fixes

--- expressions/expressions.py
+++ expressions/expressions.py
@@ -4,31 +4,51 @@ import numbers
 
 
 class Expression:
-    """Expression class represengts an algebriac expression."""
+    """Expression class."""
 
     def __init__(self, operands):
-        """Expression class constructor method."""
-        self.o = operands
+        """Initialise an expresiion."""
+        self.o = operands    
 
-    def __add__(self):
-        """Add operand 1 to operand 2."""
-        Expression(self.o)
+    def __add__(self, other):
+        """Return the Expr for the sum of this Expr and another."""
+        if isinstance(other, numbers.Number):
+            other = Number(other)
+        if isinstance(self, numbers.Number):
+            self = Number(self)
+        return Add(self, other)
 
-    def __sub__(self):
-        """Subtract operand 2 from operand 1."""
-        return Expression(self.o)
+    def __sub__(self, other):
+        """Return the Expr for the sub of this Expr and another."""
+        if isinstance(other, numbers.Number):
+            other = Number(other)
+        if isinstance(self, numbers.Number):
+            self = Number(self)
+        return Sub(self, other)
 
-    def __mul__(self):
-        """Multiply operand 1 by operand 2."""
-        return Expression(self.o)
+    def __mul__(self, other):
+        """Return the Expr for the mul of this Expr and another."""
+        if isinstance(other, numbers.Number):
+            other = Number(other)
+        if isinstance(self, numbers.Number):
+            self = Number(self)
+        return Mul(self, other)
 
-    def __div__(self):
-        """Divide operand 1 by operand 2."""
-        return Expression(self.o)
+    def __div__(self, other):
+        """Return the Expr for the div of this Expr and another."""
+        if isinstance(other, numbers.Number):
+            other = Number(other)
+        if isinstance(self, numbers.Number):
+            self = Number(self)
+        return Div(self, other)
 
-    def __pow__(self):
-        """Divide operand 1 by operand 2."""
-        return Expression(self.o)
+    def __pow__(self, other):
+        """Return the Expr for the pow of this Expr and another."""
+        if isinstance(other, numbers.Number):
+            other = Number(other)
+        if isinstance(self, numbers.Number):
+            self = Number(self)
+        return Pow(self, other)
 
 
 class Operator(Expression):
@@ -40,66 +60,82 @@ class Operator(Expression):
 
     def __str__(self):
         """Return a string representation of the Operator."""
-        return f'{self.o[0]} {self.symbol} {self.o[1]}'
+        left_str = " ".join(map(str, self.o[:len(self.o) - 2]))
+        return f'{left_str} {self.symbol} {self.o[-1]}'
+
+def make_operands_tuple(left, symbol, right):
+    """Help function"""
+    if isinstance(left, Terminal):
+        operands_list = [left.value, symbol, right.value]  
+    else:
+        operands_list = [item for item in left.o]
+        operands_list.append(symbol)
+        operands_list.append(right.value)    
+    return tuple(operands_list)
 
 
 class Add(Operator):
-    """Add class."""
+    """Addition class."""
 
     precedence = 2
     symbol = '+'
 
-    def __init__(self, x, y):
-        super().__init__((x, y))
-        super().__add__()
+    def __init__(self, left, right):
+        """Construct an Add object"""
+        self.o = make_operands_tuple(left, self.symbol, right)
+        Expression(self.o)
 
 
 class Sub(Operator):
-    """Subtraction class."""
+    """Subtract class."""
 
     precedence = 2
     symbol = '-'
 
-    def __init__(self, x, y):
-        super().__init__((x, y))
-        super().__sub__()
+    def __init__(self, left, right):
+        """Construct a Sub object"""
+        self.o = make_operands_tuple(left, self.symbol, right)
+        Expression(self.o)
 
 
 class Mul(Operator):
     """Multiply class."""
 
+    precedence = 1
     symbol = '*'
 
-    def __init__(self, x, y):
-        super().__init__((x, y))
-        super().__mul__()
+    def __init__(self, left, right):
+        """Construct a Mul object"""
+        self.o = make_operands_tuple(left, self.symbol, right)
+        Expression(self.o)
 
 
 class Div(Operator):
     """Divide class."""
 
+    precedence = 1
     symbol = '/'
 
-    def __init__(self, x, y):
-        super().__init__((x, y))
-        super().__div__()
+    def __init__(self, left, right):
+        """Construct an Div object"""
+        self.o = make_operands_tuple(left, self.symbol, right)
+        Expression(self.o)
 
 
 class Pow(Operator):
-    """power class."""
+    """Power class."""
 
+    precedence = 3
     symbol = '^'
 
-    def __init__(self, x, y):
-        super().__init__((x, y))
-        super().__pow__()
+    def __init__(self, left, right):
+        """Construct an Pow object"""
+        self.o = make_operands_tuple(left, self.symbol, right)
+        Expression(self.o)        
 
 
 class Terminal(Expression):
-    """Terminal Class."""
-
     def __init__(self, value):
-        """Operator class constructor method."""
         super().__init__(())
         self.value = value
 
@@ -111,24 +147,16 @@ class Terminal(Expression):
         """Return a string representation of the Terminal."""
         return str(self.value)
 
-
 class Number(Terminal):
-    """Number class."""
-
     def __init__(self, value):
-        """Initialise a number."""
         if isinstance(value, numbers.Number):
             super().__init__(value)
         else:
             raise ValueError
 
-
 class Symbol(Terminal):
-    """Symbol class."""
-
-    def __init__(self, op):
-        """Initialise a symbol."""
-        if isinstance(op, str):
-            super().__init__(op)
-        else:
-            raise ValueError
+    def __init__(self, value):
+      if isinstance(value, str):
+          super().__init__(value)
+      else:
+          raise ValueError
